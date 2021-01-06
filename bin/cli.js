ccb60dc5552a1775890d5efce717345e72a42e80
Vadim Filimonov <philimonovvadim@gmail.com>
2021-01-06 21:41:44 +0300

    Complete step 5: add brain even

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,12 @@
 import readlineSync from 'readline-sync';
 
+let name;
+
 export const askName = () => {
-  const name = readlineSync.question('May I have your name? ');
+  name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
 };
+
+export const getName = () => name;
 
 export default askName;
