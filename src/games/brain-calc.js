3e06a2740666d1b3b32fa1643e275eb82420a048
Vadim Filimonov <philimonovvadim@gmail.com>
2021-01-09 17:27:22 +0300

    Remove eval implementation; Minor fixes

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -4,17 +4,31 @@ const OPERATORS = ['+', '-', '*'];
 
 const getRandomInt = (max = 100) => Math.floor(Math.random() * Math.floor(max));
 
+const calcAnswer = (number1, number2, operator) => {
+  switch (operator) {
+    case '+':
+      return number1 + number2;
+    case '-':
+      return number1 - number2;
+    case '*':
+      return number1 * number2;
+    default:
+      return number1 + number2;
+  }
+};
+
 export const description = () => 'What is the result of the expression?';
 
 export default () => {
   const number1 = getRandomInt();
   const number2 = getRandomInt();
-  const operator = OPERATORS[getRandomInt(3)];
+  const operatorIndex = getRandomInt(3);
+  const operator = OPERATORS[operatorIndex];
   const expression = `${number1} ${operator} ${number2}`;
   // TODO: change eval to func
-  const correctAnswer = String(eval(expression));
+  const correctAnswer = calcAnswer(number1, number2, operator);
   console.log(`Question: ${expression}`);
-  const userAnswer = readlineSync.question('Your answer: ');
+  const userAnswer = Number(readlineSync.question('Your answer: '));
   if (userAnswer !== correctAnswer) {
     const error = { userAnswer, correctAnswer };
     throw error;
